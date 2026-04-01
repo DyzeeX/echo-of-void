335900a6a16e7a35523ac71186ac2e2a0a2ac841
DyzeeX <224779771+DyzeeX@users.noreply.github.com>
2026-04-02 01:20:46 +0300

    feat: create the SFML window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 ﻿#include <iostream>
+#include "Source/Entities/Entity.hpp"
 
-using namespace std;
+#include <SFML/Graphics.hpp>
+#include <SFML/GpuPreference.hpp>
+
+SFML_DEFINE_DISCRETE_GPU_PREFERENCE
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	Entity ent;
+	std::cout << "Hello CMake." << std::endl;
+
+
+	sf::RenderWindow window(sf::VideoMode({ 900u, 600u }), "SFML works!");
+	window.setVerticalSyncEnabled(true);
+
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
