0079784a6365ced2dd1b2858676bd24bcbebf7cd
DyzeeX <224779771+DyzeeX@users.noreply.github.com>
2026-04-08 16:14:41 +0300

    feat: load functions to every assets type

--- Source/Core/ResourceManager.cpp
+++ Source/Core/ResourceManager.cpp
@@ -1,5 +1,4 @@
 #include "ResourceManager.hpp"
 
 sf::Texture& ResourceManager::LoadTexture(const std::string& id, const std::string& filename) {	return textures.Load(id, filename); }
-
 sf::Font& ResourceManager::LoadFont(const std::string& id, const std::string& filename)       { return fonts.Load(id, filename); }
