09cf117db30baa00d55fd77b7f16c7cd6a29ed3e
DyzeeX <224779771+DyzeeX@users.noreply.github.com>
2026-04-14 15:56:18 +0300

    feat: added states base class

---
+++ Source/States/GameState.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include <Core/Config.hpp>
+
+class Application;
+
+class GameState {
+public:
+	explicit GameState(Application& app) : m_app(app) {}
+	virtual ~GameState() = default;
+
+	virtual OnEnter() {}
+	virtual OnExit()  {}
+
+	virtual void HandleEvent(const sf::Event& event) = 0;
+	virtual void FixedUpdate(float dt) {}
+	virtual void Update(float dt = Config::FIXED_DT) = 0;
+	virtual void Render(sf::RenderWindow& window) = 0;
+
+	virtual bool IsOverlay() const { return false; }
+
+protected:
+	Application m_app;
+};
